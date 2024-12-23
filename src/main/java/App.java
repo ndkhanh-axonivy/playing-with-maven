fe964358a875e0c91edf9a15fe954efde6c6e826
Khanh Nguyen <khanh.nguyenduy@axonactive.com>
2024-12-23 23:39:58 +0700

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hellooooo, Maven Release Plugin!");
+        System.out.println("Hellooooo, Maven Release Plugin! Here version 1.0.1");
     }
 }
